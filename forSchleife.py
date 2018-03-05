8a74639603494caca95de49702e4cb6ea702f879
ClaryCo <171wita01@o365.htl-leoben.at>
2018-03-05 16:48:24 +0100

    forSchleife

---
+++ forSchleife.py
@@ -0,0 +1,11 @@
+#forSchleife
+
+for i in range (1,10):
+    print ("Hallo!", end = " * " )
+
+name = "Willkommen an der HTL Leoben"
+print ()
+print (name)
+
+for z in name:
+    print("= " , end = " ")
